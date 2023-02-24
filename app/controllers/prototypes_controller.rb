0316e66487dba70fd8d21d797c95221cd7f3dc15
chikati3 <chikazawakazuhiro3@gmail.com>
2023-02-24 15:25:45 +0900

    更新機能修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -33,11 +33,10 @@ class PrototypesController < ApplicationController
   end
   
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
-      @prototype = Prototype.find(params[:id])
       render :edit
     end
   end
